479f9842bcead364189350a7e2c8c9ef74743063
maheskumaras <maheskumar@instrive.in>
2022-03-03 15:42:17 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+var router = express.Router();
+const axios = require('axios');
+
+
+const apps = {
+  devHydrameet:'https://pvc-api-dev.instrive.net/web/v1/',
+  liveHydrameet:'https://vc-api.instrive.net/web/v1/',
+  nicmeet:'',
+  railmeet:''
+}
+
+/* GET home page. */
+router.get('/recording/:env/:recording_id', async(req, res, next) => {
+  if (!(req.params.env in apps))
+  {
+    res.send({error:`There is no such environment ${req.params.env}`})
+  }
+  if (req.params.recording_id =="" || req.params.recording_id ==" " )
+  {
+    res.send({error:`Recording lookup id should not be empty ${req.params.env}`})
+  }
+
+  await axios.get(`${apps[req.params.env]}meeting-recording-lookup/for-download/${req.params.recording_id}?tenantId=${req.query.tenantId}&meeting_access_token=${req.query.meeting_access_token}`).then((axiosResp)=>{
+    if (!('url' in axiosResp.data))
+    {
+      res.send(axiosResp.data)
+    }else
+    {
+      res.download('./recordings/'+axiosResp.data.url);
+    }
+  }).catch(function (error) {
+    if (error.response) {
+      res.send(error.response.data)
+    } else if (error.request) {
+      res.send(error.request)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+        res.send(error.message)
+    }
+
+  });
+  
+
+
+});
+
+module.exports = router;
